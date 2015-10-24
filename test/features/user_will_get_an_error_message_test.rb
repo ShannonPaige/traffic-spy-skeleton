b988cd9bf4a0e9bdfa3128dd76dbede67ac51a82
joshuajhun <joshuajhun@gmail.com>
2015-10-24 15:59:54 -0600

    stuff stuff

--- test/features/user_will_get_an_error_message_test.rb
+++ test/features/user_will_get_an_error_message_test.rb
@@ -19,18 +19,18 @@ class ErrorTest < FeatureTest
     visit '/sources/test_company_1'
     assert_equal'/sources/error', current_path
     within("#error li:first") do
-    assert page.has_content?("error")
+    assert page.has_content?("No user given.")
     end
   end
 
     def test_user_will_get_an_error_for_no_event
-       User.create("rootUrl" => "HTTP://Example1.com")
-       Payload.create({url: "http://jumpstartlab.com/page",
+      
+      User.create("identifier" => "test_company_1", "rootUrl" => "HTTP://Example1.com")
+      Payload.create({url: "http://jumpstartlab.com/page",
                        requestedAt: "2013-02-16 21:38:28 -0700",
                        respondedIn: 45,
                        referredBy: "http://jumpstartlab.com",
                        requestType: "GET",
-
                        userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
                        resolutionWidth: "1920",
                        resolutionHeight: "1280",
@@ -39,8 +39,8 @@ class ErrorTest < FeatureTest
                        ip: "63.29.38.211"})
       visit '/sources/test_company_1/events'
       assert_equal'/sources/error', current_path
-      within("#error li:first") do
-      assert page.has_content?("error")
+      within("#error li:last") do
+      assert page.has_content?("Error: No given Event Name in payload.")
     end
   end
 end
